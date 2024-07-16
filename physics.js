c1bffdb9c5509785e52bbbd37e80d954c9497286
Samuel <70766878+swzwij@users.noreply.github.com>
2024-07-17 01:16:11 +0200

    small gravity tweak

--- physics.js
+++ physics.js
@@ -105,8 +105,8 @@ window.addEventListener('devicemotion', function (event)
     const acceleration = event.accelerationIncludingGravity;
     const accelVector = new Vector(-acceleration.x || 0, acceleration.y || 0);
 
-    gravity.x = accelVector.x * 0.03;
-    gravity.y = accelVector.y * 0.03;
+    gravity.x = accelVector.x * 0.05;
+    gravity.y = accelVector.y * 0.05;
 
     gravityText.textContent = `${gravity.x.toFixed(2)}, ${gravity.y.toFixed(2)}`;
 
